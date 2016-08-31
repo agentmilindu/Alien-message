c28b5a8ab056a93ed0e4ad1f65528b3357c7dda6
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2016-08-31 09:27:04 +0530

    Initial

---
+++ alien.py
@@ -0,0 +1,19 @@
+import png
+
+def n_plicate(original_list, n):
+    return reduce(lambda x,y: x+y, map(lambda x: [x]*n, original_list))
+
+l = "00110000011000111111110110010011001001100101111000100100010010001001001100110"
+m = []
+
+m = [ n_plicate(l[i:i+7], 100) for i in range(0,len(l), 7)]
+
+m = n_plicate(m, 100)
+
+img = open('alien.png', 'wb')
+
+palette=[(0x55,0x55,0x55), (0xff,0x99,0x99)]
+
+w = png.Writer(700,1100, palette=palette, bitdepth=1)
+
+w.write(img, m)
